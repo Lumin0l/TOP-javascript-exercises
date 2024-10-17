388eaf9122cd64bdbb7e068f1e98566173d91214
Imanol <idelaiglesia.m@gmail.com>
2024-10-17 10:49:29 +0200

    first couple

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,26 @@
-const repeatString = function() {
+const { error } = require("console");
+
+const repeatString = function(str, n) {
+    let finalString = "";
+    
+    if (n < 0) {
+        return ("ERROR");
+    }
+
+    for (let i = n; i > 0; i--) {
+        
+        finalString = finalString + str;
+        /* finalString = finalString.concat("", str);
+        console.log(finalString); */
+    }
+
+    
+    /* console.log(finalString); */
+    return (finalString);
 
 };
+
+/* repeatString("blin", 3); */
 
 // Do not edit below this line
 module.exports = repeatString;
